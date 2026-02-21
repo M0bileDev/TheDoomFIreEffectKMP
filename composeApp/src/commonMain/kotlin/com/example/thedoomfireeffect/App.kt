a1fa6d6eea84d05417e51b825f9354f8797a0337
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-22 00:35:48 +0100

    feature(main): add loop

--- composeApp/src/commonMain/kotlin/com/example/thedoomfireeffect/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/thedoomfireeffect/App.kt
@@ -3,30 +3,47 @@ package com.example.thedoomfireeffect
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameMillis
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import kotlin.random.Random
 
 @Composable
-@Preview
 fun App() {
+
+    var state by remember { mutableStateOf(DoomState(offset = 0.25f)) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            withFrameMillis {
+                val newOffset = Random.nextDouble(0.2, 0.4)
+                state = state.copy(offset = newOffset.toFloat())
+            }
+        }
+    }
+
+    DoomCompose(state)
+
 }
 
 @Composable
-fun DoomCompose() {
-
-    val offsetX = 0.25f
-    val offsetY = 0.25f
+fun DoomCompose(state: DoomState) = with(state) {
 
     Canvas(modifier = Modifier.fillMaxSize()) {
         drawRect(
             color = Color.Red,
-            topLeft = Offset(x = size.width * offsetX, y = size.height * offsetY),
+            topLeft = Offset(x = size.width * offset, y = size.height * offset),
             size = Size(
-                width = size.width * (1f - offsetX * 2f),
-                height = size.height * (1f - offsetY * 2f)
+                width = size.width * (1f - offset * 2f),
+                height = size.height * (1f - offset * 2f)
             )
         )
     }
@@ -35,5 +52,9 @@ fun DoomCompose() {
 @Preview
 @Composable
 fun PreviewDoomCompose() {
-    DoomCompose()
+    DoomCompose(DoomState(offset = 0.25f))
 }
+
+data class DoomState(
+    val offset: Float
+)
