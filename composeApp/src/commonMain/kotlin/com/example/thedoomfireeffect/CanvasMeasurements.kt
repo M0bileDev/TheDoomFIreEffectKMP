2c5286bad5978d2a23b197a8145761042dd2da38
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-23 01:09:56 +0100

    refactor(main): extract value to constant

--- composeApp/src/commonMain/kotlin/com/example/thedoomfireeffect/CanvasMeasurements.kt
+++ composeApp/src/commonMain/kotlin/com/example/thedoomfireeffect/CanvasMeasurements.kt
@@ -2,6 +2,8 @@ package com.example.thedoomfireeffect
 
 import kotlin.math.ceil
 
+const val NUMBER_OF_COLUMNS = 50
+
 data class CanvasMeasurements(
     val width: Int = 0,
     val height: Int = 0
@@ -13,17 +15,17 @@ val CanvasMeasurements.tallerThanWide: Boolean
 val CanvasMeasurements.pixelSize: Int
     get() {
         val longestLength = if (tallerThanWide) width else height
-        return ceil(longestLength.toDouble() / 50).toInt()
+        return ceil(longestLength.toDouble() / NUMBER_OF_COLUMNS).toInt()
     }
 
 val CanvasMeasurements.widthPixel: Int
     get() = when {
-        tallerThanWide -> 50
+        tallerThanWide -> NUMBER_OF_COLUMNS
         else -> ceil(width.toDouble() / pixelSize).toInt()
     }
 
 val CanvasMeasurements.heightPixel: Int
     get() = when {
-        !tallerThanWide -> 50
+        !tallerThanWide -> NUMBER_OF_COLUMNS
         else -> ceil(height.toDouble() / pixelSize).toInt()
     }
